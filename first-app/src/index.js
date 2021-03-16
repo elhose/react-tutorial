6da9748ab6b67e5432f9b6dd91c1ab32b7c9561f
Jacek <jaceksawko95@gmail.com>
2021-03-16 22:49:12 +0100

    Added dummy eventHandlers, just to show, that the should be called using inline () => operator and then referenced aas function ( passing just refference might initialize handler on React component creation)

--- first-app/src/index.js
+++ first-app/src/index.js
@@ -40,16 +40,33 @@ function BookList() {
 
 const Book = (props) => {
 	const { img, title, author } = props;
+	// attribute, eventHandler
+	// onClick, onMouseOver
+	const clickHandler = () => {
+		alert("Button was clicked!");
+	};
+
+	const complexExample = (author) => {
+		console.log(`LOGGED AUTHOR IS ${author}`);
+	};
 	return (
-		<article className="book">
+		<article className="book" onMouseOver={() => {
+			console.log(`TITLE IS ${title}`);
+		}}>
 			<img
 				src={img}
 				alt="Example for a book"
 				// width="200"
 				height="280"
 			/>
-			<h2>{title}</h2>
+			<h2 onClick={() => console.log(title)}>{title}</h2>
 			<h4>{author}</h4>
+			<button type="button" onClick={clickHandler}>
+				Reference Example
+			</button>
+			<button type="button" onClick={() => complexExample(author)}>
+				More Complete Example
+			</button>
 		</article>
 	);
 };

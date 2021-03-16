7bb564ae5c566db773dd34520686b84554284fd1
Jacek <jaceksawko95@gmail.com>
2021-03-16 19:14:32 +0100

    Merged all components to one, moved texts to variables

--- first-app/src/index.js
+++ first-app/src/index.js
@@ -23,28 +23,23 @@ function BookList() {
 	);
 }
 
+const author = "Sam Lloyd";
 const Book = () => {
+  const title = "Schronisko";
 	return (
 		<article className="book">
-			<Image></Image>
-			<Title />
-			<Author />
+			<img
+				src="https://www.swiatksiazki.pl/media/catalog/product/cache/eaf55611dc9c3a2fa4224fad2468d647/7/2/7299906720572.jpg"
+				alt="Example for a book"
+				// width="200"
+				height="280"
+			/>
+			<h2>{title}</h2>
+			<h4 >{author}</h4>
+      {/* <p>{let x =6}</p> */}
+      <p>{1+2}</p>
 		</article>
 	);
 };
-
-const Image = () => (
-	<img
-		src="https://www.swiatksiazki.pl/media/catalog/product/cache/eaf55611dc9c3a2fa4224fad2468d647/7/2/7299906720572.jpg"
-		alt="Example for a book"
-		// width="200"
-		height="280"
-	/>
-);
-
-const Title = () => <h2>Schronisko</h2>;
-
-const authorStyling = { color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" };
-const Author = () => <h4 style={authorStyling}>Sam Lloyd</h4>;
 
 ReactDom.render(<BookList />, document.getElementById("root"));

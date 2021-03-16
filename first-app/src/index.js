a7218b3d87497990ce46439231f5b1126d0d3f13
Jacek <jaceksawko95@gmail.com>
2021-03-16 22:29:42 +0100

    Refactored hardcoded books to use map arrow function, used spread operator to pass props to component

--- first-app/src/index.js
+++ first-app/src/index.js
@@ -4,45 +4,42 @@ import ReactDom from "react-dom";
 //CSS
 import "./index.css";
 
-const firstBook = {
-	img:
-		"https://www.swiatksiazki.pl/media/catalog/product/cache/eaf55611dc9c3a2fa4224fad2468d647/7/2/7299906720572.jpg",
-	title: "Schronisko",
-	author: "Sam Lloyd",
-};
-
-const secondBook = {
-	img:
-		"https://www.swiatksiazki.pl/media/catalog/product/cache/eaf55611dc9c3a2fa4224fad2468d647/6/3/6399906730463.jpg",
-	title: "Sekta",
-	author: "Marek Stelar",
-};
+const books = [
+	{
+		id: 1,
+		img:
+			"https://www.swiatksiazki.pl/media/catalog/product/cache/eaf55611dc9c3a2fa4224fad2468d647/7/2/7299906720572.jpg",
+		title: "Schronisko",
+		author: "Sam Lloyd",
+	},
+	{
+		id: 2,
+		img:
+			"https://www.swiatksiazki.pl/media/catalog/product/cache/eaf55611dc9c3a2fa4224fad2468d647/6/3/6399906730463.jpg",
+		title: "Sekta",
+		author: "Marek Stelar",
+	},
+	{
+		id: 3,
+		img:
+			"https://www.swiatksiazki.pl/media/catalog/product/cache/eaf55611dc9c3a2fa4224fad2468d647/8/3/8399906720183.jpg",
+		title: "Później",
+		author: "Stephen King",
+	},
+];
 
 function BookList() {
 	return (
 		<section className="booklist">
-			<Book
-				img={firstBook.img}
-				title={firstBook.title}
-				author={firstBook.author}
-			>
-				<p>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit.
-					Vero ut perferendis quia temporibus quas eum impedit
-					deserunt rerum? Soluta, quam?
-				</p>
-			</Book>
-			<Book
-				img={secondBook.img}
-				title={secondBook.title}
-				author={secondBook.author}
-			/>
+			{books.map((book) => {
+				return <Book key={book.id} {...book} />;
+			})}
 		</section>
 	);
 }
 
 const Book = (props) => {
-	const {img, title, author} = props
+	const { img, title, author } = props;
 	return (
 		<article className="book">
 			<img
@@ -53,7 +50,6 @@ const Book = (props) => {
 			/>
 			<h2>{title}</h2>
 			<h4>{author}</h4>
-			{props.children}
 		</article>
 	);
 };

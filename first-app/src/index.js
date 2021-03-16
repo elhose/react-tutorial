d5d6ef84c6ffa1bb495a503963ff257af576047c
Jacek <jaceksawko95@gmail.com>
2021-03-16 19:24:01 +0100

    Clean up, move all variables to const in class

--- first-app/src/index.js
+++ first-app/src/index.js
@@ -4,40 +4,31 @@ import ReactDom from "react-dom";
 //CSS
 import "./index.css";
 
+const title = "Schronisko";
+const author = "Sam Lloyd";
+const imgSrc =
+	"https://www.swiatksiazki.pl/media/catalog/product/cache/eaf55611dc9c3a2fa4224fad2468d647/7/2/7299906720572.jpg";
+
+  
 function BookList() {
 	return (
 		<section className="booklist">
-			<Book />
-			<Book />
-			<Book />
-			<Book />
-			<Book />
-			<Book />
-			<Book />
-			<Book />
-			<Book />
-			<Book />
-			<Book />
 			<Book />
 		</section>
 	);
 }
 
-const author = "Sam Lloyd";
 const Book = () => {
-  const title = "Schronisko";
 	return (
 		<article className="book">
 			<img
-				src="https://www.swiatksiazki.pl/media/catalog/product/cache/eaf55611dc9c3a2fa4224fad2468d647/7/2/7299906720572.jpg"
+				src={imgSrc}
 				alt="Example for a book"
 				// width="200"
 				height="280"
 			/>
 			<h2>{title}</h2>
-			<h4 >{author}</h4>
-      {/* <p>{let x =6}</p> */}
-      <p>{1+2}</p>
+			<h4>{author}</h4>
 		</article>
 	);
 };

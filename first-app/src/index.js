1e622f9eefca1f9c29594d0dd9df2ec6cba71fbb
Jacek <jaceksawko95@gmail.com>
2021-03-16 19:49:59 +0100

    Introduced passing parameters as props in components (parameter in Component should be called props -> it's convention)

--- first-app/src/index.js
+++ first-app/src/index.js
@@ -4,31 +4,49 @@ import ReactDom from "react-dom";
 //CSS
 import "./index.css";
 
-const title = "Schronisko";
-const author = "Sam Lloyd";
-const imgSrc =
-	"https://www.swiatksiazki.pl/media/catalog/product/cache/eaf55611dc9c3a2fa4224fad2468d647/7/2/7299906720572.jpg";
+const firstBook = {
+	img:
+		"https://www.swiatksiazki.pl/media/catalog/product/cache/eaf55611dc9c3a2fa4224fad2468d647/7/2/7299906720572.jpg",
+	title: "Schronisko",
+	author: "Sam Lloyd",
+};
 
-  
+const secondBook = {
+	img:
+		"https://www.swiatksiazki.pl/media/catalog/product/cache/eaf55611dc9c3a2fa4224fad2468d647/6/3/6399906730463.jpg",
+	title: "Sekta",
+	author: "Marek Stelar",
+};
+
 function BookList() {
 	return (
 		<section className="booklist">
-			<Book />
+			<Book
+				img={firstBook.img}
+				title={firstBook.title}
+				author={firstBook.author}
+			/>
+			<Book
+				img={secondBook.img}
+				title={secondBook.title}
+				author={secondBook.author}
+			/>
 		</section>
 	);
 }
 
-const Book = () => {
+const Book = (props) => {
+	console.log(props);
 	return (
 		<article className="book">
 			<img
-				src={imgSrc}
+				src={props.img}
 				alt="Example for a book"
 				// width="200"
 				height="280"
 			/>
-			<h2>{title}</h2>
-			<h4>{author}</h4>
+			<h2>{props.title}</h2>
+			<h4>{props.author}</h4>
 		</article>
 	);
 };

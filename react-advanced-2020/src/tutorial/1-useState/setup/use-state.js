c87a6a313a24a903d041803b07feb64c6ed0045b
Jacek <jaceksawko95@gmail.com>
2021-03-22 23:02:28 +0100

    Used functional approach for complex counter

--- react-advanced-2020/src/tutorial/1-useState/setup/use-state.js
+++ react-advanced-2020/src/tutorial/1-useState/setup/use-state.js
@@ -8,6 +8,13 @@ const UseState = () => {
     setValue(0);
   };
 
+  const complexIncrease = () => {
+    setTimeout(()=>{
+      // setValue(val+1)
+      setValue((previousState) => previousState + 1);
+    },2000);
+  }
+
   return <React.Fragment>
     <section style={{margin: '4rem 0'}}>
       <h2>regular counter</h2>
@@ -19,7 +26,7 @@ const UseState = () => {
     <section style={{margin: '4rem 0'}}>
       <h2>complex counter</h2>
       <h1>{value}</h1>
-      <button className="btn" onClick={() => setValue(value - 1)}>complex increase</button>
+      <button className="btn" onClick={() => complexIncrease()}>complex increase</button>
     </section>
   </React.Fragment>;
 };

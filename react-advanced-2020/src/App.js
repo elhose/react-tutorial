ac3345927d070bae43b892746286e948c4d5d47d
Jacek <jaceksawko95@gmail.com>
2021-03-27 18:17:01 +0100

    Prepared app for useEffect tutorial part

--- react-advanced-2020/src/App.js
+++ react-advanced-2020/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Setup from './tutorial/1-useState/setup/use-state'
+import Setup from './tutorial/2-useEffect/setup/use-effect'
 function App() {
   return (
     <div className='container'>
---
+++ react-advanced-2020/src/tutorial/2-useEffect/setup/use-effect.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+function UseEffect() {
+    return <h1>Hello!</h1>
+}
+
+export default UseEffect

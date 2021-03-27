e0cc861cab9055582e6a5c43acdb8681b0492733
Jacek <jaceksawko95@gmail.com>
2021-03-27 18:08:02 +0100

    Finish of birthday reminder project

--- 1-birthday-reminder/src/App.js
+++ 1-birthday-reminder/src/App.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import data from './data'
+import List from './List'
 
 function App() {
-  return <h2>Hello!</h2>
+  const [people, setPeople] = useState(data);
+  const size = people.length;
+  const deletePeople = () => setPeople([])
+
+  return <main>
+    <section className="container">
+      <h3>{size} birthdays today!</h3>
+      <List people={people}/>
+      <button onClick={() => deletePeople()}>Clear all</button>
+    </section>
+  </main>
 }
 
 export default App;

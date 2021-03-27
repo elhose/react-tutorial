e6811675ba71aa734cce8733c4d721f34061516f
Jacek <jaceksawko95@gmail.com>
2021-03-27 20:16:06 +0100

    UseEffect allows for hooking up function, that will be run after every rerender of the component

--- react-advanced-2020/src/tutorial/2-useEffect/setup/use-effect.js
+++ react-advanced-2020/src/tutorial/2-useEffect/setup/use-effect.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
 function UseEffect() {
-    return <h1>Hello!</h1>
+	const [value, setValue] = useState(0);
+	useEffect(() => {
+		console.log("useEffect called!");
+    document.title = `New Messages ${value}`
+	});
+
+	console.log("render component");
+	return (
+		<>
+			<h1>{value}</h1>
+			<button className="btn" onClick={() => setValue(value + 1)}>
+				Click me!
+			</button>
+		</>
+	);
 }
 
-export default UseEffect
+export default UseEffect;

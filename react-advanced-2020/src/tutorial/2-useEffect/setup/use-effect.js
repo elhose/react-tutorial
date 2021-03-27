5f9403a366a6f54b312b4cbde67449eaffc89737
Jacek <jaceksawko95@gmail.com>
2021-03-28 00:24:42 +0100

    Example of data fetching using fetch js API and useEffect

--- react-advanced-2020/src/tutorial/2-useEffect/setup/use-effect.js
+++ react-advanced-2020/src/tutorial/2-useEffect/setup/use-effect.js
@@ -1,23 +1,36 @@
 import React, { useState, useEffect } from "react";
 
+const url = "https://api.github.com/users";
+
 function UseEffect() {
-	const [size, setSize] = useState(window.innerWidth);
-	const checkSize = () => setSize(window.innerWidth);
-
+	const [users, setUsers] = useState([]);
+	const getUsers = async () => {
+		const response = await fetch(url);
+		const users = await response.json();
+		setUsers(users);
+		console.log(users);
+	};
 	useEffect(() => {
-		console.log("useEffect");
-		window.addEventListener("resize", checkSize);
+		getUsers();
+	}, []);
 
-		return () => {
-			console.log("cleanup");
-			window.removeEventListener("resize", checkSize);
-		};
-	});
-	console.log("render");
 	return (
 		<>
-			<h1>window</h1>
-			<h2>{size} PX</h2>
+			<h3>github users: {users.length}</h3>
+			<ul className="users">
+				{users.map((user) => {
+					const { id, login, avatar_url, html_url } = user;
+					return (
+						<li key={id}>
+							<img src={avatar_url} alt={login} />
+							<div>
+								<h4>{login}</h4>
+								<a href={html_url}>Git Profile</a>
+							</div>
+						</li>
+					);
+				})}
+			</ul>
 		</>
 	);
 }

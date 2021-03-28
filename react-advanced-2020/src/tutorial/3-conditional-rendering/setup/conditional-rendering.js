48695bc485b3dad01c68bb7c8578e8869707365e
Jacek <jaceksawko95@gmail.com>
2021-03-28 12:03:03 +0200

    Example of conditional rendering using fetch with Promises

--- react-advanced-2020/src/tutorial/3-conditional-rendering/setup/conditional-rendering.js
+++ react-advanced-2020/src/tutorial/3-conditional-rendering/setup/conditional-rendering.js
@@ -1,13 +1,57 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+
+const url = "https://api.github.com/users/QuincyLarsonXD";
 
 function ConditionalRendering() {
-	const [loading, setLoading] = useState(true);
+	const [isLoading, setIsLoading] = useState(true);
+	const [isError, setIsError] = useState(false);
+	const [user, setUser] = useState("default User");
 
-	if (loading) {
-		return <h2>Loading...</h2>;
+	useEffect(() => {
+		fetch(url)
+			.then((response) => {
+				if (response.status === 200) {
+					return response.json();
+				} else {
+					setIsLoading(false);
+					setIsError(true);
+					throw new Error(response.statusText);
+				}
+			})
+			.then((user) => {
+				const { login } = user;
+				setUser(login);
+				setIsLoading(false);
+				setIsError(false);
+				console.log(user);
+			})
+			.catch((error) => {
+				console.log(error);
+				setIsError(true);
+			});
+	}, []);
+
+	if (isLoading) {
+		return (
+			<>
+				<h1>Loading...</h1>
+			</>
+		);
 	}
 
-	return <h2>Multiple Returns</h2>;
+	if (isError) {
+		return (
+			<>
+				<h1>Error !!!</h1>
+			</>
+		);
+	}
+
+	return (
+		<>
+			<h1>{user}</h1>
+		</>
+	);
 }
 
 export default ConditionalRendering;

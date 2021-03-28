cae5ad31e956d19c5acbcfb041a74d9bbaa0d592
Jacek <jaceksawko95@gmail.com>
2021-03-28 13:58:02 +0200

    Ternary operator is the same as in Java

--- react-advanced-2020/src/tutorial/3-conditional-rendering/setup/conditional-rendering.js
+++ react-advanced-2020/src/tutorial/3-conditional-rendering/setup/conditional-rendering.js
@@ -1,17 +1,24 @@
 import React, { useEffect, useState } from "react";
 
 function ConditionalRendering() {
-	const [text, setText] = useState("Peter");
-	const firstValue = text || "hello world";
-	const secondValue = text && "hello world";
+	const [text, setText] = useState("");
+	const [isError, setIsError] = useState(false);
+	// const firstValue = text || "hello world";
+	// const secondValue = text && "hello world";
 
 	return (
 		<>
-			{/* <h1>short-circuit evaluation with ternary operation</h1> */}
-      {/* <h1>First value is -&gt; {firstValue}</h1> */}
-      {/* <h1>Second value is -&gt; {secondValue}</h1> */}
-      <h1>{text || "john doe"}</h1>
-      <h1>{text && "Emilly Jones"}</h1>
+			<h1>{text || "john doe"}</h1>
+			<button
+				className="btn"
+				onClick={() => {
+					setIsError(!isError);
+				}}
+			>
+				toggle error
+			</button>
+			<h1>{isError && "Gotcha!"}</h1>
+			<h1>{isError ? "Gotcha!" : "The same behaviour using ternary operator"}</h1>
 		</>
 	);
 }

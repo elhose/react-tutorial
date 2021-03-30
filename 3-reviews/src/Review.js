7df8ecb78c3c76273f4dd8dab4aff2d5997254db
Jacek <jaceksawko95@gmail.com>
2021-03-31 00:06:56 +0200

    Installed & Imported react icons

--- 3-reviews/src/Review.js
+++ 3-reviews/src/Review.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState } from "react";
+import {FaChevronLeft, FaChevronRight, FaQuoteRight} from "react-icons/fa"
 
 const Review = () => {
     return (

8deb7bf31b431c27ce09f0391cf116051f5a5381
Jacek <jaceksawko95@gmail.com>
2021-04-05 00:03:53 +0200

    Installed react-icons, imported necessary icons

--- 7-slider/src/App.js
+++ 7-slider/src/App.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+import { FaQuoteRight } from "react-icons/fa";
 import data from "./data";
 
 function App() {

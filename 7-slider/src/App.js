9ff160a3f2d0e8c69cab634fbb0a2bfd416f4fe8
Jacek <jaceksawko95@gmail.com>
2021-04-05 00:20:25 +0200

    Implemented Main visuals of a slide

--- 7-slider/src/App.js
+++ 7-slider/src/App.js
@@ -4,8 +4,30 @@ import { FaQuoteRight } from "react-icons/fa";
 import data from "./data";
 
 function App() {
-  const [people, setPeople] = useState(data);
-	return <h2>slider project setup</h2>;
+	const [people, setPeople] = useState(data);
+	const [index, setIndex] = useState(0);
+
+	const { id, image, name, title, quote } = people[index];
+	return (
+		<>
+			<section className="section">
+				<div className="title">
+					<h2>
+						<span>/</span>Reviews
+					</h2>
+				</div>
+			</section>
+			<section className="section-center">
+				<article>
+					<img className="person-img" src={image} alt={name} />
+					<h4>{name}</h4>
+					<h5>{title}</h5>
+					<p className="text">{quote}</p>
+					<FaQuoteRight className="icon" />
+				</article>
+			</section>
+		</>
+	);
 }
 
 export default App;

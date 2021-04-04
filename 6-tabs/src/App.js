40bb1229a278583695b41710cdf97689d91f74cb
Jacek <jaceksawko95@gmail.com>
2021-04-04 23:01:28 +0200

    Implemented buttons for each job

--- 6-tabs/src/App.js
+++ 6-tabs/src/App.js
@@ -4,7 +4,7 @@ const url = "https://course-api.com/react-tabs-project";
 
 function App() {
 	const [initialized, setInitialized] = useState(false);
-	const [data, setData] = useState([]);
+	const [jobs, setJobs] = useState([]);
 	useEffect(() => {
 		fetchData();
 	}, []);
@@ -14,25 +14,42 @@ function App() {
 			const response = await fetch(url);
 			const body = await response.json();
 			setInitialized(true);
-			setData(body);
+			setJobs(body);
 		} catch (err) {
 			setInitialized(false);
 			console.log(err);
 		}
 	};
 
-	console.log(data);
+	console.log(jobs);
 
 	if (!initialized) {
 		return <h1 className="loading">LOADING ...</h1>;
 	}
 
 	return (
-		<main>
-			<header className="title">
-				<h1>Experience</h1>
+		<main className="section">
+			<section className="title">
+				<h2>Experience</h2>
 				<div className="underline"></div>
-			</header>
+			</section>
+			<section className="job-center">
+				<div className="btn-container">
+					{jobs.map((entry) => {
+						const { id, company } = entry;
+						return (
+							<button
+								key={id}
+								className="job-btn"
+								type="button"
+							>
+								{company}
+							</button>
+						);
+					})}
+				</div>
+				<div className="job-info"></div>
+			</section>
 		</main>
 	);
 }

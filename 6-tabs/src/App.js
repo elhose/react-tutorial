2edc5f33a5f7f4a8321cb848798f4af0e120e462
Jacek <jaceksawko95@gmail.com>
2021-04-04 22:46:57 +0200

    Implemented data fetchiung and simple initialization

--- 6-tabs/src/App.js
+++ 6-tabs/src/App.js
@@ -1,7 +1,40 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
+
+const url = "https://course-api.com/react-tabs-project";
 
 function App() {
-  return <h2>tabs project setup</h2>
+	const [initialized, setInitialized] = useState(false);
+	const [data, setData] = useState([]);
+	useEffect(() => {
+		fetchData();
+	}, []);
+
+	const fetchData = async () => {
+		try {
+			const response = await fetch(url);
+			const body = await response.json();
+			setInitialized(true);
+			setData(body);
+		} catch (err) {
+			setInitialized(false);
+			console.log(err);
+		}
+	};
+
+	console.log(data);
+
+	if (!initialized) {
+		return <h1 className="loading">LOADING ...</h1>;
+	}
+
+	return (
+		<main>
+			<header className="title">
+				<h1>Experience</h1>
+				<div className="underline"></div>
+			</header>
+		</main>
+	);
 }
 
-export default App
+export default App;

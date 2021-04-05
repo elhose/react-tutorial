30f3852dd47b83b60186ac508c041b67616f2a88
Jacek <jaceksawko95@gmail.com>
2021-04-05 23:15:08 +0200

    Setup button handler, and jsx basics

--- 9-color-generator/src/App.js
+++ 9-color-generator/src/App.js
@@ -1,8 +1,43 @@
-import React, { useState } from 'react'
-import Values from "values.js"
+import React, { useState } from "react";
+import Values from "values.js";
 
 function App() {
-  return <h2>color generator project</h2>
+	const [color, setColor] = useState("");
+	const [error, setError] = useState(false);
+	const [list, setList] = useState([]);
+
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		console.log("hello");
+	};
+
+	return (
+		<>
+			<section className="container">
+				<h3>color generator</h3>
+				<form>
+					<input
+						type="text"
+						value={color}
+						onChange={(event) => {
+							setColor(event.target.value);
+						}}
+						placeholder="#f15025"
+					/>
+					<button
+						className="btn"
+						type="submit"
+						onClick={handleSubmit}
+					>
+						Submit
+					</button>
+				</form>
+			</section>
+			<section className="colors">
+				<h4>here goes list</h4>
+			</section>
+		</>
+	);
 }
 
-export default App
+export default App;

68e3c74ea863afa4d84e274937bd1fcb81bb4d52
Jacek <jaceksawko95@gmail.com>
2021-04-05 14:15:51 +0200

    Implemented correct animation css class in article

--- 7-slider/src/App.js
+++ 7-slider/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
 import { FaQuoteRight } from "react-icons/fa";
 import data from "./data";
@@ -14,15 +14,25 @@ function App() {
 		}
 		setIndex(index + 1);
 	};
+
 	const prevPerson = () => {
 		if (index - 1 < 0) {
-			setIndex(people.length-1);
+			setIndex(people.length - 1);
 			return;
 		}
 		setIndex(index - 1);
 	};
 
-	const { id, image, name, title, quote } = people[index];
+	const determineAnimationClass = (personIndex) => {
+		if (personIndex === index) {
+			return "activeSlide";
+		}
+		if (personIndex === index - 1 || (index === 0 && personIndex === people.length)) {
+			return "lastSlide";
+		}
+		return "nextSlide";
+	};
+
 	return (
 		<>
 			<section className="section">
@@ -33,13 +43,27 @@ function App() {
 				</div>
 			</section>
 			<section className="section-center">
-				<article>
-					<img className="person-img" src={image} alt={name} />
-					<h4>{name}</h4>
-					<p className="title">{title}</p>
-					<p className="text">{quote}</p>
-					<FaQuoteRight className="icon" />
-				</article>
+				{people.map((person, personIndex) => {
+					const { id, image, name, title, quote } = person;
+
+					return (
+						<article
+							key={id}
+							className={determineAnimationClass(personIndex)}
+						>
+							<img
+								className="person-img"
+								src={image}
+								alt={name}
+							/>
+							<h4>{name}</h4>
+							<p className="title">{title}</p>
+							<p className="text">{quote}</p>
+							<FaQuoteRight className="icon" />
+						</article>
+					);
+				})}
+
 				<button className="prev" onClick={() => prevPerson()}>
 					<FiChevronLeft />
 				</button>

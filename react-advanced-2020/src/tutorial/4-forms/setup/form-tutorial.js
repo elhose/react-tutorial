0b9e8b56e5d643a3c4165e73a7140eb4f8d8ddd1
Jacek <jaceksawko95@gmail.com>
2021-04-05 14:02:03 +0200

    Form basics

--- react-advanced-2020/src/tutorial/4-forms/setup/form-tutorial.js
+++ react-advanced-2020/src/tutorial/4-forms/setup/form-tutorial.js
@@ -6,8 +6,28 @@ import React, { useState } from "react";
 // value, onChange
 
 const ControlledInputs = () => {
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		console.log("Hello!", event);
+	};
+
 	return (
 		<>
+			<article>
+				<form className="form" onSubmit={handleSubmit}>
+					<div className="form-control">
+						<label htmlFor="firstName">Name: </label>
+						<input type="text" id="firstName" name="firstName" />
+					</div>
+					<div className="form-control">
+						<label htmlFor="email">Email: </label>
+						<input type="text" id="email" name="email" />
+					</div>
+					<button type="submit" onClick={handleSubmit}>
+						add person
+					</button>
+				</form>
+			</article>
 		</>
 	);
 };
